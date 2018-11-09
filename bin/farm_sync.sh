022c45603f6cc1906d7259448afc6295317d1af0
allydunham <ally@ebi.ac.uk>
2018-11-09 13:06:11 +0000

    Added script to automate (relatively) safe rsync between laptop and farm

---
+++ bin/farm_sync.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+# Script to sync data and meta folders between farm and local folder
+# Currently does not delete things - must be done manually for safety
+
+#Colours for printf
+green=$(tput setaf 2)
+normal=$(tput sgr0)
+
+## Sync To Farm
+# Data folder
+printf "%s\n%s\n%s\n" "${green}Rsyncing mutations project${normal}" "${green}Local -> Farm${normal}" "${green}File List: Data/${normal}"
+rsync -vauh --exclude-from $HOME/.rsync_exclude  --dry-run $HOME/Projects/mutations/data/ ebi:/nfs/research1/beltrao/ally/mutations/data
+
+read -p "Transfer? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   rsync -auh --info=progress2 --exclude-from $HOME/.rsync_exclude $HOME/Projects/mutations/data/ ebi:/nfs/research1/beltrao/ally/mutations/data
+fi
+
+#Meta folder
+printf "\n%s\n" "${green}Files List: Meta/${normal}"
+rsync -vauh --exclude-from $HOME/.rsync_exclude  --dry-run $HOME/Projects/mutations/meta/ ebi:/nfs/research1/beltrao/ally/mutations/meta
+
+read -p "Transfer? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   rsync -auh --info=progress2 --exclude-from $HOME/.rsync_exclude $HOME/Projects/mutations/meta/ ebi:/nfs/research1/beltrao/ally/mutations/meta
+fi
+
+## Sync From Farm
+printf "\n%s\n" "${green}Farm -> Local${normal}"
+# Data Folder
+printf "%s\n" "${green}File List: Data${normal}"
+rsync -vauh --exclude-from $HOME/.rsync_exclude  --dry-run ebi:/nfs/research1/beltrao/ally/mutations/data/ $HOME/Projects/mutations/data
+
+read -p "Transfer? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   rsync -auh --info=progress2 --exclude-from $HOME/.rsync_exclude ebi:/nfs/research1/beltrao/ally/mutations/data/ $HOME/Projects/mutations/data
+fi
+
+# Meta Folder
+printf "\n\n%s\n" "${green}Files List: Meta${normal}"
+rsync -vauh --exclude-from $HOME/.rsync_exclude  --dry-run ebi:/nfs/research1/beltrao/ally/mutations/meta/ $HOME/Projects/mutations/meta
+
+read -p "Transfer? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   rsync -auh --info=progress2 --exclude-from $HOME/.rsync_exclude ebi:/nfs/research1/beltrao/ally/mutations/meta/ $HOME/Projects/mutations/meta
+fi
+
