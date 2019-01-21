34825f03b2dd5908f5556ac098848583cd731451
allydunham <ally@ebi.ac.uk>
2019-01-21 15:38:12 +0000

    Modified foldx_cat script to take right file head size

--- bin/foldx_cat.sh
+++ bin/foldx_cat.sh
@@ -4,21 +4,20 @@
 PDB_ID=$1
 
 # Process Average
-head -n 1 Average_0_$PDB_ID.fxout > Average_$PDB_ID.fxout
-tail -n +2 -q Average_*_$PDB_ID.fxout >> Average_$PDB_ID.fxout
+head -n 9 Average_0_$PDB_ID.fxout > Average_$PDB_ID.fxout
+tail -n +10 -q Average_*_$PDB_ID.fxout >> Average_$PDB_ID.fxout
 #rm Average_*_$PDB_ID.fxout
 
 # Process Dif
-head -n 1 Dif_0_$PDB_ID.fxout > Dif_$PDB_ID.fxout
-tail -n +2 -q Dif_*_$PDB_ID.fxout >> Dif_$PDB_ID.fxout
+head -n 9 Dif_0_$PDB_ID.fxout > Dif_$PDB_ID.fxout
+tail -n +10 -q Dif_*_$PDB_ID.fxout >> Dif_$PDB_ID.fxout
 #rm Dif_*_$PDB_ID.fxout
 
 # Process Raw
-head -n 1 Raw_0_$PDB_ID.fxout > Raw_$PDB_ID.fxout
-tail -n +2 -q Raw_*_$PDB_ID.fxout >> Raw_$PDB_ID.fxout
+head -n 9 Raw_0_$PDB_ID.fxout > Raw_$PDB_ID.fxout
+tail -n +10 -q Raw_*_$PDB_ID.fxout >> Raw_$PDB_ID.fxout
 #rm Raw_*_$PDB_ID.fxout
 
 # Process PdbList
-head -n 1 PdbList_0_$PDB_ID.fxout > PdbList_$PDB_ID.fxout
-tail -n +2 -q PdbList_*_$PDB_ID.fxout >> PdbList_$PDB_ID.fxout
+cat PdbList_*_$PDB_ID.fxout >> PdbList_$PDB_ID.fxout
 #rm PdbList_*_$PDB_ID.fxout
