d1f4ab12fa8eeea1ead34cdcdaa58b7c13af47b3
allydunham <ally@ebi.ac.uk>
2019-01-23 14:02:29 +0000

    Fixed paths

--- bin/foldx_cat.sh
+++ bin/foldx_cat.sh
@@ -4,20 +4,20 @@
 PDB_ID=$1
 
 # Process Average
-head -n 9 Average_0_$PDB_ID.fxout > Average_$PDB_ID.fxout
-tail -n +10 -q Average_*_$PDB_ID.fxout >> Average_$PDB_ID.fxout
-#rm Average_*_$PDB_ID.fxout
+head -n 9 Average_0_${PDB_ID}_repair.fxout > Average_${PDB_ID}_repair.fxout
+tail -n +10 -q Average_*_${PDB_ID}_repair.fxout >> Average_$PDB_ID.fxout
+#rm Average_*_${PDB_ID}_repair.fxout
 
 # Process Dif
-head -n 9 Dif_0_$PDB_ID.fxout > Dif_$PDB_ID.fxout
-tail -n +10 -q Dif_*_$PDB_ID.fxout >> Dif_$PDB_ID.fxout
-#rm Dif_*_$PDB_ID.fxout
+head -n 9 Dif_0_${PDB_ID}_repair.fxout > Dif_${PDB_ID}_repair.fxout
+tail -n +10 -q Dif_*_${PDB_ID}_repair.fxout >> Dif_${PDB_ID}_repair.fxout
+#rm Dif_*_${PDB_ID}_repair.fxout
 
 # Process Raw
-head -n 9 Raw_0_$PDB_ID.fxout > Raw_$PDB_ID.fxout
-tail -n +10 -q Raw_*_$PDB_ID.fxout >> Raw_$PDB_ID.fxout
-#rm Raw_*_$PDB_ID.fxout
+head -n 9 Raw_0_${PDB_ID}_repair.fxout > Raw_${PDB_ID}_repair.fxout
+tail -n +10 -q Raw_*_${PDB_ID}_repair.fxout >> Raw_${PDB_ID}_repair.fxout
+#rm Raw_*_${PDB_ID}_repair.fxout
 
 # Process PdbList
-cat PdbList_*_$PDB_ID.fxout >> PdbList_$PDB_ID.fxout
-#rm PdbList_*_$PDB_ID.fxout
+cat PdbList_*_${PDB_ID}_repair.fxout >> PdbList_${PDB_ID}_repair.fxout
+#rm PdbList_*_${PDB_ID}_repair.fxout
