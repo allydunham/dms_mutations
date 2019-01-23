4d2e266212def66142a1a5784719805cf717bfe6
allydunham <ally@ebi.ac.uk>
2019-01-23 14:04:40 +0000

    Fixed 0 indexing of files

--- bin/foldx_cat.sh
+++ bin/foldx_cat.sh
@@ -4,17 +4,17 @@
 PDB_ID=$1
 
 # Process Average
-head -n 9 Average_0_${PDB_ID}_repair.fxout > Average_${PDB_ID}_repair.fxout
+head -n 9 Average_1_${PDB_ID}_repair.fxout > Average_${PDB_ID}_repair.fxout
 tail -n +10 -q Average_*_${PDB_ID}_repair.fxout >> Average_$PDB_ID.fxout
 #rm Average_*_${PDB_ID}_repair.fxout
 
 # Process Dif
-head -n 9 Dif_0_${PDB_ID}_repair.fxout > Dif_${PDB_ID}_repair.fxout
+head -n 9 Dif_1_${PDB_ID}_repair.fxout > Dif_${PDB_ID}_repair.fxout
 tail -n +10 -q Dif_*_${PDB_ID}_repair.fxout >> Dif_${PDB_ID}_repair.fxout
 #rm Dif_*_${PDB_ID}_repair.fxout
 
 # Process Raw
-head -n 9 Raw_0_${PDB_ID}_repair.fxout > Raw_${PDB_ID}_repair.fxout
+head -n 9 Raw_1_${PDB_ID}_repair.fxout > Raw_${PDB_ID}_repair.fxout
 tail -n +10 -q Raw_*_${PDB_ID}_repair.fxout >> Raw_${PDB_ID}_repair.fxout
 #rm Raw_*_${PDB_ID}_repair.fxout
 
