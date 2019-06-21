0d34c76ee67fa103adf7ed8d280865110fc6f574
allydunham <ally@ebi.ac.uk>
2019-06-21 18:07:33 +0100

    Fixed bug where height was read as units when plotting all PCs

--- bin/r_modules/utility_functions.R
+++ bin/r_modules/utility_functions.R
@@ -1,6 +1,22 @@
 #!/usr/bin/env Rscript 
 # Script containing small utility functions used throughout the project
 
+# Convert a subset of a tibble to a matrix with labeled rows
+tibble_to_matrix <- function(tbl, columns, row_names=NULL){
+  columns <- enquo(columns)
+  if (!is.null(row_names)){
+    if (length(row_names) == 1 & is.character(row_names)){
+      row_names <- tbl[[row_names]]
+    }
+  }
+  tbl <- select(tbl, !!columns) %>%
+    as.matrix()
+  if (!is.null(row_names)){
+    tbl <- set_rownames(tbl, row_names)
+  }
+  return(tbl)
+}
+
 # Convert AA encoding between 1 letter/3 letter
 AA_CODE_1_TO_3 <- AMINO_ACID_CODE
 AA_CODE_3_TO_1 <- structure(names(AMINO_ACID_CODE), names=AMINO_ACID_CODE)
@@ -29,7 +45,7 @@ plot_all_pcs <- function(tbl, max_pc=20, colour_var='wt', nrow=2, ncol=5, width 
           }),
         nrow = nrow, ncol = ncol, common.legend = TRUE, legend = 'right'
       ),
-      width = ifelse(is.null(width), ncol * 4, width), ifelse(is.null(height), nrow * 4, height)
+      width = ifelse(is.null(width), ncol * 4, width), height = ifelse(is.null(height), nrow * 4, height)
     )
   )
 }
