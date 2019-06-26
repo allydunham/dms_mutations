6f7f403c180021faf5a9dbcd635c6669f0fbf32d
allydunham <ally@ebi.ac.uk>
2019-06-26 12:42:43 +0100

    Added function to scatterplot a list of factors

--- src/util/misc_util.R
+++ src/util/misc_util.R
@@ -1,6 +1,37 @@
 #!/usr/bin/env Rscript 
 # Script containing small utility functions used throughout the project
 
+#### raw computation ####
+# returns a list with a vector of unique row labels and a vector of bools as from duplicated(mat)
+# combined these can map rowwise results from de-duplicated matrix back to original as int labels correspond to
+# row numbers in de-duped matrix
+enumerate_unique_rows <- function(mat){
+  dupe_rows <- duplicated(mat)
+  
+  unique_inds <- rep(0, length(dupe_rows))
+  ind <- 1
+  for (i in 1:length(dupe_rows)){
+    if (!dupe_rows[i]){
+      # enumerate non-dupe rows with unused index
+      unique_inds[i] <- ind
+      ind <- ind + 1
+      
+    } else if (unique_inds[i] == 0) {
+      # If its a dupe row, set indeces to first occurance (which will have been previously set)
+      dupe_row_nums <- which(apply(mat, 1, identical, y=mat[i,]))
+      unique_inds[dupe_row_nums] <- unique_inds[dupe_row_nums[1]]
+    }
+  }
+  return(list(indeces=unique_inds, duplicate=dupe_rows))
+}
+
+# Given a matrix return ind
+find_matching_rows <- function(target, mat){
+  apply(mat, 1, identical, y=target)
+}
+
+########
+
 #### Tibbles ####
 # Convert a subset of a tibble to a matrix with labeled rows
 tibble_to_matrix <- function(tbl, columns, row_names=NULL){
@@ -59,5 +90,15 @@ plot_all_pcs <- function(tbl, max_pc=20, colour_var='wt', nrow=2, ncol=5, width 
       width = ifelse(is.null(width), ncol * 4, width), height = ifelse(is.null(height), nrow * 4, height)
     )
   )
+}
+
+# Plot scatter plots against a list of factors (supplied as symbols)
+plot_factors <- function(tbl, x, y, factors){
+  x = enquo(x)
+  y = enquo(y)
+  plist <- sapply(factors,
+                  function(fac){ggplot(tbl, aes(x=!!x, y=!!y, colour=!!fac)) + geom_point()},
+                  simplify = FALSE)
+  return(plist)
 }
 ########
