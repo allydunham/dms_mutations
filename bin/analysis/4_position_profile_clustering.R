a05c8b63c8fbf029457262a2ff5e25a595b2c03e
allydunham <ally@ebi.ac.uk>
2019-07-04 18:00:27 +0100

    Added some tidied presentation plots

--- bin/analysis/4_position_profile_clustering.R
+++ bin/analysis/4_position_profile_clustering.R
@@ -28,15 +28,46 @@ plots <- list_modify(plots, pca=sapply(variant_pcas, function(x){list(aa_pca_pro
                                        simplify = FALSE))
 
 # Corelation between PCs and surface accesibility
-pca_surf_cor <- sapply(variant_pcas, pca_surf_acc_cor, simplify = FALSE, factor_cols = all_atom_abs:polar_rel)
+pca_surf_cor <- sapply(variant_pcas, pca_factor_cor, simplify = FALSE, .vars = vars(all_atom_abs:polar_rel))
 
-plots <- list_modify(plots, pca=sapply(pca_surf_cor, function(x){list(pca_surf_acc_cor_heatmap=pca_surface_heatmap(x))}, simplify = FALSE))
+plots <- list_modify(plots, pca=sapply(pca_surf_cor, function(x){list(pca_surf_acc_cor_heatmap=pca_factor_heatmap(x))}, simplify = FALSE))
 
-# Per AA PCAs #
-AAs <- sort(unique(variant_matrices$all_variants$wt))
-plots <- list_modify(plots, pca=sapply(imputed_matrices, function(x){
-  list(per_aa_pcas=sapply(AAs, per_aa_pcas, variant_matrix=x, simplify = FALSE))
-}, simplify = FALSE))
+# # Per AA PCAs #
+# AAs <- sort(unique(variant_matrices$all_variants$wt))
+# plots <- list_modify(plots, pca=sapply(imputed_matrices, function(x){
+#   list(per_aa_pcas=sapply(AAs, per_aa_pcas, variant_matrix=x, simplify = FALSE))
+# }, simplify = FALSE))
+########
+
+#### Tidy plots ####
+plots$pca$sig_positions$poster_surf_acc <- ggplot(drop_na(variant_pcas$sig_positions$profiles, all_atom_abs),
+                                                  aes(x=PC2, y=PC4, colour=all_atom_abs)) +
+  scale_colour_gradientn(colours = c('blue', 'green', 'yellow', 'orange', 'red')) +
+  geom_point() +
+  lims(x=c(-5,5), y=c(-4,4)) +
+  theme_pubclean() +
+  theme(legend.position = 'right', panel.grid.major = element_line(colour = 'lightgrey', linetype = 'dotted')) +
+  guides(colour=guide_colorbar(title='Surface\nAccesibility'))
+
+plots$pca$sig_positions$poster_sig_count <- ggplot(variant_pcas$sig_positions$profiles,
+                                                   aes(x=PC1, y=PC2, colour=sig_count)) +
+  scale_colour_gradientn(colours = c('blue', 'red')) +
+  geom_point() +
+  theme_pubclean() +
+  theme(legend.position = 'right', panel.grid.major = element_line(colour = 'lightgrey', linetype = 'dotted')) +
+  guides(colour=guide_colorbar(title='# Significant\nSubstitutions'))
+
+# Comparison between avg AA PCA profile and blosum62
+data("BLOSUM62")
+
+aa_prof_blosum <- as_tibble(BLOSUM62, rownames='AA1') %>%
+  gather(key = 'AA2', value = 'BLOSUM62', -AA1) %>%
+  filter(AA1 %in% Biostrings::AA_STANDARD, AA2 %in% Biostrings::AA_STANDARD) %>%
+  left_join(., avg_AA_pca_profiles$sig_positions$cor_tbl, by=c('AA1', 'AA2'))
+
+plots$pca$sig_positions$avg_aa_profile_blosum_cor <- ggplot(aa_prof_blosum, aes(x=cor, y=BLOSUM62)) +
+  geom_point()
+
 ########
 
 save_plot_list(plots, root='figures/4_position_profile_clustering/')
