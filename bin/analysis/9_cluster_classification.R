039348281f6389f36fee6ca5a780d61e4d4db3ca
allydunham <ally@ebi.ac.uk>
2019-08-21 17:07:36 +0100

    Collected variables to use for classification

--- bin/analysis/9_cluster_classification.R
+++ bin/analysis/9_cluster_classification.R
@@ -6,3 +6,12 @@ source('src/analysis/cluster_classification.R')
 
 library(caret)
 
+dm_hclust <- readRDS('data/rdata/deep_mut_hclust_clusters.RDS')
+
+# Narrow down data to useful columns
+dm_tbl <- select(dm_hclust$tbl, cluster, study, gene_name, position=pos, wt, A:Y, all_atom_rel, ss, phi, psi) %>%
+  left_join(., select(dm_hclust$foldx, cluster, study, position=pos, wt, total_energy:entropy_complex),
+            by = c('cluster', 'study', 'position', 'wt')) %>%
+  select(-total_energy, -sloop_entropy, -mloop_entropy, -entropy_complex, -electrostatic_kon, -water_bridge) # drop unused foldx terms 
+
+# Classify 
