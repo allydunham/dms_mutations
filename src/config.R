358d259bf0a9fcd7907226d4deb7d01e08c4ff95
allydunham <ally@ebi.ac.uk>
2019-09-26 17:18:13 +0100

    Added format_study function and reworked it to be more elegant

--- src/config.R
+++ src/config.R
@@ -65,3 +65,39 @@ MANUAL_THRESHOLDS <- c(araya_2012_yap1=-2, ashenberg_2016_nucleoprotein=-1, bren
 # ggplot theme (currently unset)
 theme_set(theme_gray()) 
 
+# Function to format study names
+format_study <- function(x, max_width=60){
+  spl <- str_split(x, '[_.]')[[1]]
+  
+  # Year
+  year <- spl[2]
+  
+  # Authour
+  if (all(spl[1:2] == c('roscoe', '2014'))){
+    authour <- 'Roscoe & Bolon'
+  } else {
+    authour <- str_c(str_to_title(spl[1]), ' et al.')
+  }
+  
+  # Gene
+  gene <- spl[3]
+  if (gene %in% c('nucleoprotein', 'proteing')){
+    gene <- c(nucleoprotein='NP', proteing='ProteinG')[gene]
+  } else if (gene == 'aph3'){
+    gene <- 'APH3-II'
+    spl <- spl[c(1, 2, 3, 5:length(spl))] # remove erroneously split ii
+  } else {
+    gene <- str_to_upper(gene)
+  }
+  
+  # Condition
+  condition <- NA
+  if (!is.na(spl[4])){
+    condition <- str_c(spl[4:length(spl)], collapse = ' ')
+  }
+  
+  result <- str_c(authour, ' ', year, ' (', gene, ifelse(is.na(condition), '', str_c(', ', condition)), ')') %>%
+    str_wrap(width = max_width)
+  
+  return(result)
+}
