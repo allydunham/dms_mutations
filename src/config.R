bcdeb4ed59b048cb2978a46f7340ee0b32975f21
allydunham <ally@ebi.ac.uk>
2021-06-10 17:45:29 +0100

    Added blank plot and pretty p values utility functions

--- src/config.R
+++ src/config.R
@@ -101,3 +101,39 @@ format_study <- function(x, max_width=60){
   
   return(result)
 }
+
+pretty_p_values <- function(p, breaks = c(0.001, 0.01, 0.05), markdown_exp=FALSE, prefix_p=FALSE){
+  breaks <- sort(breaks, decreasing = TRUE)
+  break_str <- as.character(breaks)
+  gt <- '>'
+  lt <- '<'
+  
+  if (markdown_exp){
+    break_str <- str_replace(break_str, "^1?e(-?[0-9]*\\.?[0-9]*)", "10<sup>\\1</sup>")
+    gt <- '&gt;'
+    lt <- '&lt;'
+  }
+  
+  p_out <- rep(str_c(gt, ' ', break_str[1]), length(p))
+  for (i in 1:length(breaks)){
+    p_out[p < breaks[i]] <- str_c(lt, ' ', break_str[i])
+  }
+  break_levels <- c(str_c(gt, ' ', break_str[1]), unlist(map(break_str, ~str_c(lt, ' ', .))))
+  
+  if (prefix_p){
+    p_out <- str_c('p ', p_out)
+    break_levels <- str_c('p ', break_levels)
+  }
+  
+  return(factor(p_out, levels = break_levels))
+}
+
+blank_plot <- function(text = ''){
+  ggplot(tibble(x=c(0, 1)), aes(x=x, y=x)) +
+    geom_blank() +
+    annotate(geom = 'text', x = 0.5, y = 0.5, label = text) +
+    theme(panel.grid.major.y = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text = element_blank(),
+          axis.title = element_blank())
+}
